5b43a6627e05220c221a6ed5e3104e008ce6c4a6
MOHAMED MOHANA <mohammedommuhanna@gmail.com>
2026-02-03 15:37:35 +0300

    Add correct SHA256 hash for v1.0.0 release

--- Casks/openports.rb
+++ Casks/openports.rb
@@ -1,8 +1,9 @@
 cask "openports" do
   version "1.0.0"
-  sha256 :no_check
+  sha256 "13d2d51133abc6d99786ebae059d789b6a255b2438e0ed8a3856b1a6852e2730"
 
-  url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-#{version}.zip"
+  url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-#{version}.zip",
+      verified: "github.com/MohamedMohana/openports/"
   name "OpenPorts"
   desc "Lightweight macOS menu bar app for monitoring local ports and processes"
   homepage "https://github.com/MohamedMohana/openports"
