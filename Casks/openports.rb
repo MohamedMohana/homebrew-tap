5fd875bb3201861c00407d560c72fb1b0929495a
MOHAMED MOHANA <mohammedommuhanna@gmail.com>
2026-02-04 14:17:57 +0300

    bump: update openports to v1.1.2

--- Casks/openports.rb
+++ Casks/openports.rb
@@ -1,8 +1,8 @@
 cask "openports" do
   version "1.1.2"
-  sha256 "a4caa16d5ad0500d517f9017384eb5f8924fb19df846bdf6b44df1832474a13c"
+  sha256 :no_check
 
-  url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}.zip",
+  url "https://github.com/MohamedMohana/openports/releases/download/v\#{version}/OpenPorts-v\#{version}.zip",
       verified: "github.com/MohamedMohana/openports/"
   name "OpenPorts"
   desc "Lightweight macOS menu bar app for monitoring local ports and processes with safety ratings and Preferences UI"
