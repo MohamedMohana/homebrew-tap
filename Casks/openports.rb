ebe37b91e427838244212292cce638ea712d0f15
MOHAMED MOHANA <mohammedommuhanna@gmail.com>
2026-02-05 13:08:11 +0300

    Update checksum for v1.1.3 Preferences

--- Casks/openports.rb
+++ Casks/openports.rb
@@ -1,6 +1,6 @@
 cask "openports" do
   version "1.1.3"
-  sha256 "c24e5134fc3f529683bf935b97cb986ecc003ff2594b008c417af996769b039c"
+  sha256 "d5d3f3dfb34801548f5e56e7dd21f1345c68f1353963feb61e9a868c1ca343ff"
 
   url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}.zip",
       verified: "github.com/MohamedMohana/openports/"
