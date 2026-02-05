cad524dfd13e6f65626777dc77896552cb4feadd
MOHAMED MOHANA <mohammedommuhanna@gmail.com>
2026-02-05 11:54:40 +0300

    Update openports to v1.1.3

--- Casks/openports.rb
+++ Casks/openports.rb
@@ -1,6 +1,6 @@
 cask "openports" do
-  version "1.1.2"
-  sha256 :no_check
+  version "1.1.3"
+  sha256 "116614ba537f94135d725c81d996061761bb85c2e7b72f2b0bcd30efa6b50f48"
 
   url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}.zip",
       verified: "github.com/MohamedMohana/openports/"
