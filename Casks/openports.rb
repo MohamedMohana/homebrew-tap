67957d80ea24e6cbc88c9988b458bd381d1ab088
MOHAMED MOHANA <mohammedommuhanna@gmail.com>
2026-03-03 08:46:41 +0300

    openports 2.0.4

--- Casks/openports.rb
+++ Casks/openports.rb
@@ -1,16 +1,16 @@
 cask "openports" do
-  version "2.0.3"
+  version "2.0.4"
 
   on_arm do
-    url "https://github.com/MohamedMohana/openports/releases/download/v2.0.3/OpenPorts-v2.0.3-arm64.zip",
+    url "https://github.com/MohamedMohana/openports/releases/download/v2.0.4/OpenPorts-v2.0.4-arm64.zip",
         verified: "github.com/MohamedMohana/openports/"
-    sha256 "f340ae8b2ee658fb457df93e73d918613315f5536e83f1422c5ff9f02ed81b53"
+    sha256 "6d96293877fb83317956f19382274e5089d2c59b4b70982315ad1f0ad616d9bc"
   end
 
   on_intel do
-    url "https://github.com/MohamedMohana/openports/releases/download/v2.0.3/OpenPorts-v2.0.3-intel.zip",
+    url "https://github.com/MohamedMohana/openports/releases/download/v2.0.4/OpenPorts-v2.0.4-intel.zip",
         verified: "github.com/MohamedMohana/openports/"
-    sha256 "671afdaa6dbe17fba7f2d5aa7c41852dfbd9157900e56c27258bd2c5af45808c"
+    sha256 "98094d105762e4879fe314f7ad7530db63ed3823c7d7e4b512e1f33362e99676"
   end
 
   name "OpenPorts"
