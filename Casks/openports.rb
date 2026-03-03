fc7e442e38aa784f8b3f15325916665710413266
MOHAMED MOHANA <mohammedommuhanna@gmail.com>
2026-03-03 09:12:25 +0300

    Update openports to v2.0.5

--- Casks/openports.rb
+++ Casks/openports.rb
@@ -1,20 +1,20 @@
 cask "openports" do
-  version "2.0.4"
+  version "2.0.5"
 
   on_arm do
-    url "https://github.com/MohamedMohana/openports/releases/download/v2.0.4/OpenPorts-v2.0.4-arm64.zip",
+    url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}-arm64.zip",
         verified: "github.com/MohamedMohana/openports/"
-    sha256 "6d96293877fb83317956f19382274e5089d2c59b4b70982315ad1f0ad616d9bc"
+    sha256 "32560965198716b61248852f3e4e94713289e1ebd1a1fc103b2aad55ef151929"
   end
 
   on_intel do
-    url "https://github.com/MohamedMohana/openports/releases/download/v2.0.4/OpenPorts-v2.0.4-intel.zip",
+    url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}-intel.zip",
         verified: "github.com/MohamedMohana/openports/"
-    sha256 "98094d105762e4879fe314f7ad7530db63ed3823c7d7e4b512e1f33362e99676"
+    sha256 "cd9f596dd12d04139c618cbb3d4b03f6bd2de57be39edce272beded62e3e862c"
   end
 
   name "OpenPorts"
-  desc "Lightweight macOS menu bar app for monitoring local ports and processes with safety ratings and real-time preference updates"
+  desc "Smart port monitoring for Mac developers"
   homepage "https://github.com/MohamedMohana/openports"
 
   auto_updates false
