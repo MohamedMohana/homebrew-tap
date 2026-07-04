edd83c34cb7d67c03b51c12f655a3715a7a78a87
MOHAMED MOHANA <mohammedommuhanna@gmail.com>
2026-07-04 04:05:49 +0300

    Update openports to 2.2.0

--- Casks/openports.rb
+++ Casks/openports.rb
@@ -1,16 +1,16 @@
 cask "openports" do
-  version "2.1.1"
+  version "2.2.0"
 
   on_arm do
     url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}-arm64.zip",
         verified: "github.com/MohamedMohana/openports/"
-    sha256 "4ce0dc290e5a15cb053e5b2c96148fc6729ddf56c128da205aa19a8d197270c5"
+    sha256 "e08f8855ad0fa9936ade11dc7335240224c19b72d827f73c21ccb47f6408fd8c"
   end
 
   on_intel do
     url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}-intel.zip",
         verified: "github.com/MohamedMohana/openports/"
-    sha256 "0611e6e0283ad95de2f8c833ee2d65ec79e1701b9aede9fa92c53dd6c2b6f4bc"
+    sha256 "e53139b8850518b4a2492db9647761dae35e6fc735ab564ec78dee486d1d5946"
   end
 
   name "OpenPorts"
@@ -18,7 +18,7 @@ cask "openports" do
   homepage "https://github.com/MohamedMohana/openports"
 
   auto_updates false
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "OpenPorts.app"
 end
