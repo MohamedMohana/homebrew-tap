5cd3c1709576e85ceabe433e4ff8f41fcdfb33ca
MOHAMED MOHANA <mohammedommuhanna@gmail.com>
2026-09-17 20:13:33 +0300

    Fix deprecated OpenPorts cask URL verification

--- Casks/openports.rb
+++ Casks/openports.rb
@@ -2,14 +2,12 @@ cask "openports" do
   version "2.5.1"
 
   on_arm do
-    url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}-arm64.zip",
-        verified: "github.com/MohamedMohana/openports/"
+    url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}-arm64.zip"
     sha256 "1300c7e6292be662ef5da305d7798d8affa2ad5bb91464335f1ccfb6ff91e420"
   end
 
   on_intel do
-    url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}-intel.zip",
-        verified: "github.com/MohamedMohana/openports/"
+    url "https://github.com/MohamedMohana/openports/releases/download/v#{version}/OpenPorts-v#{version}-intel.zip"
     sha256 "26cf084319ab6c011d2c416b0dbc1a86e3c31acdbadcdcaab95554d48d1cb7e5"
   end
 
